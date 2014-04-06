c8c527fd2372ffaff517797dc8996bb8f5276226
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2014-04-06 02:54:49 -0400

    Added some basic SoundCloud API commands.

--- js/soundcloud.playa.js
+++ js/soundcloud.playa.js
@@ -1,36 +1,84 @@
 var SOUNDCLOUD_CLIENT = "8232a972fc27da0d122b41211218729f"
-var SOUNDCLOUD_SECRET = "8bbad1fd360dce19be13d05102d55985"
+
+var tracks = [
+	"https://soundcloud.com/arthyum/red-hot-chili-peppers",
+	"https://soundcloud.com/dualseize/suicide-doors",
+	"https://soundcloud.com/octobersveryown/drake-days-in-the-east"
+]
 
 SC.initialize({
   client_id: SOUNDCLOUD_CLIENT,
   redirect_uri: "http://127.0.0.1:3000/callback.html",
 });
 
-SC.get("/groups/55517/tracks", {limit: 1}, function(tracks){
-  alert("Latest track: " + tracks[0].title);
+SC.oEmbed(tracks[0], {auto_play: true}, function(oembed){
+	$(function() {
+		$('body').prepend(oembed.html);
+	})
 });
 
 
-var playing = false;
-
-$('#play').click(function() {
-	if(playing){
-		pause();
-		playing = false;
-
-	}else{
-		play();
-		playing = true;
-	}
-	
-	
+tracks.forEach(function(track) {
+	getSCURI(track, function(streamTrack) {
+		var trackListHTML = '<a href="#" class="list-group-item" data-track-id="' + streamTrack.id + '">' + streamTrack.title + '</a>'
+		$('#trackList').append(trackListHTML)
+	})
 })
+
+var playing = false;
+var currentTrackID = 0;
+
+
+
+
+/**
+ * curl -v 'http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/matas/hobnotropic&client_id=YOUR_CLIENT_ID'
+ * Takes a SoundCloud URL and converts it into URI for streaming
+ */
+function getSCURI(url, callback) {
+
+	$.ajax({
+		url: "http://api.soundcloud.com/resolve.json?url=" + url + "&client_id=" + SOUNDCLOUD_CLIENT,
+		success: function(track) {
+			callback(track) 
+		}
+	})
+
+}
 
 function pause(){
 
 
 }
-function play(){
 
-
+function play(trackID){
+	currentTrackID = trackID;
+	SC.stream("/tracks/" + currentTrackID, {
+	  autoPlay: true,
+	  ontimedcomments: function(comments){
+	    console.log(comments[0].body);
+	  }
+	});
 }
+
+$(document).ready(function() {
+
+	$(document).on('click', '#trackList a', function() {
+		play($(this).data('track-id'))
+	})
+
+
+	$('#play').click(function() {
+		if(playing){
+			pause();
+			playing = false;
+
+		}else{
+			if (currentTrackID == 0) 
+				currentTrackID = $('#trackList a').data('track-id')
+			play(currentTrackID);
+			playing = true;
+		}
+	})
+})
+
