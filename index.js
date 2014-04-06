9842507ce399c02a5a59d9c2bf4b444878dceac9
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2014-04-06 01:28:49 -0400

    Added SoundCloud API.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-var PORT = 3000;
+var PORT = 3000
+var SOUNDCLOUD_CLIENT = "8232a972fc27da0d122b41211218729f"
+var SOUNDCLOUD_SECRET = "8bbad1fd360dce19be13d05102d55985"
 
 app.get('/', function(req, res){
-  res.sendfile('views/index.html');
-});
+  res.sendfile('views/index.html')
+})
 
 console.log("Listening on port " + PORT)
 app.listen(PORT)
