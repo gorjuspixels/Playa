8a01c967d5e7e3d6aa5f3c5ecf68de755dd54bfc
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2014-04-07 21:17:24 -0400

    Removed typo.

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function streamTrack(trackID) {
 	for(var i=0; i<NUMBER_OF_TRACKS; i++) {
 		var track = tracks[i]
 		if (track.id == trackID) {
-			io.sockets.emit('nowPlaying', { "title": track.title, "artist": track.user.username, "trackID": track._.uniqueId([prefix])});
+			io.sockets.emit('nowPlaying', { "title": track.title, "artist": track.user.username, "trackID": track.id});
 			break;
 		}
 	}
